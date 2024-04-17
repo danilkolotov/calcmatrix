4c11d69c976f6b80ad26a704c938bad0da2012d6
Danil Kolotov <danilkolotov@mail.ru>
2024-04-17 04:05:26 +0300

    initial

---
+++ src/main/java/calcmatrix/CalcmatrixApplication.java
@@ -0,0 +1,13 @@
+package calcmatrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcmatrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalcmatrixApplication.class, args);
+    }
+
+}
