0cf1c7c21b0679882f3394d14f52bdc7f4fc7939
Danil Kolotov <danilkolotov@mail.ru>
2024-04-18 04:37:07 +0300

    id auto generation

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,3 +1,3 @@
-insert into query(id, n, operation) values (2, 2, 0);
-insert into query(id, n, operation) values (3, 2, 1);
-insert into query(id, n, operation) values (4, 2, 1);
+insert into query(n, operation) values (2, 0);
+insert into query(n, operation) values (2, 1);
+insert into query(n, operation) values (2, 1);
